add5d323aa329bd93e91c36bd298ab83f56bd2dd
AlexEhtNordi <ali.ehteshami.bejnordi@est.fib.upc.edu>
2021-06-27 14:00:29 +0200

    define dataloader

---
+++ dataloader.py
@@ -0,0 +1,26 @@
+from torch.utils.data import Dataset
+
+
+class TrainData(Dataset):
+    def __init__(self, x_data, y_data):
+        self.x_data = x_data
+        self.y_data = y_data
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return len(self.x_data)
+
+
+class TestData(Dataset):
+    def __init__(self, X_data):
+        self.x_data = X_data
+
+    def __getitem__(self, index):
+        return self.x_data[index]
+
+    def __len__(self):
+        return len(self.x_data)
+
+
